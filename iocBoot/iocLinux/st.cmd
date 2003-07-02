92d182ed4ae0b309b6d83b996aa3fc5b2c1191cd
Mark Rivers <MarkRivers@users.noreply.github.com>
2003-07-02 17:49:02 +0000

    New syntax for many xxxConfig commands

--- iocBoot/iocLinux/st.cmd
+++ iocBoot/iocLinux/st.cmd
@@ -7,10 +7,7 @@
 #
 dbLoadDatabase("../../dbd/mcaCanberra.dbd",0,0)
 registerRecordDeviceDriver(pdbbase) 
-dbLoadRecords("../../mcaApp/Db/mca.db","P=mcaTest:,M=aim_adc1,NCHAN=2048,NUSE=2048,DTYPE=MPF MCA,INP=#C0 S0@AIMServ")
-dbLoadRecords("../../mcaApp/Db/icb_adc.db", "P=mcaTest:,ADC=adc1,CARD=0,SERVER=icb/1,ADDR=0")
-dbLoadRecords("../../mcaApp/Db/icb_tca.db", "P=mcaTest:,TCA=tca1,MCA=aim_adc1,CARD=0,SERVER=icbTca/1,ADDR=0")
-#dbLoadRecords("../../../std/stdApp/Db/scan.db","P=13LAB:,MAXPTS1=2000,MAXPTS2=200,MAXPTS3=20,MAXPTS4=10,MAXPTSH=10")
+#dbLoadRecords("../../../std/stdApp/Db/scan.db","P=mcaTest:,MAXPTS1=2000,MAXPTS2=200,MAXPTS3=20,MAXPTS4=10,MAXPTSH=10")
 dbLoadRecords("../../../std/stdApp/Db/pid_control.db","P=mcaTest:,PID=PID1,INP=test,OUT=test,SCAN=0,KP=1.,KI=1.,KD=0.,LOPR=0,DRVL=0,HOPR=100.,DRVH=100.,PREC=3")
 
 #
@@ -25,11 +22,22 @@ localMessageRouterStart(0)
 #          maxSignals, maxSequences, ethernetDevice, queueSize)
 AIMConfig("AIMServ", 0x59e, 1, 2048, 1, 1, "eth1", 1000)
 mcaAIMShowModules()
+dbLoadRecords("../../mcaApp/Db/mca.db","P=mcaTest:,M=aim_adc1,NCHAN=2048,NUSE=2048,DTYPE=MPF MCA,INP=#C0 S0@AIMServ")
 
-#icbConfig(serverName, maxModules, address, queueSize)
-icbConfig("icb/1", 10, "NI59E:5", 100)
+#icbSetup(serverName, maxModules, queueSize)
+icbSetup("icb/1", 10, 100)
+#icbConfig(serverName, module, ethernetAddress, icbAddress)
+icbConfig("icb/1", 0, 0x59e, 5)
+dbLoadRecords("../../../mca/mcaApp/Db/icb_adc.db", "P=13LAB:,ADC=adc1,CARD=0,SERVER=icb/1,ADDR=0")
+icbConfig("icb/1", 1, 0x59e, 3)
+dbLoadRecords("../../../mca/mcaApp/Db/icb_amp.db", "P=13LAB:,AMP=amp1,CARD=0,SERVER=icb/1,ADDR=1")
+icbConfig("icb/1", 2, 0x59e, 2)
+dbLoadRecords("../../../mca/mcaApp/Db/icb_hvps.db", "P=13LAB:,HVPS=hvps1,CARD=0,SERVER=icb/1,ADDR=2, LIMIT=1000")
 
-#icbTcaConfig(serverName, maxModules, address, queueSize)
-icbTcaConfig("icbTca/1", 1, "NI59E:8", 100)
+#icbTcaSetup(serverName, maxModules, queueSize)
+icbTcaSetup("icbTca/1", 10, 100)
+#icbTcaConfig(serverName, module, ethernetAddress, icbAddress)
+icbTcaConfig("icbTca/1", 0, 0x59e, 8)
+dbLoadRecords("../../../mca/mcaApp/Db/icb_tca.db", "P=13LAB:,TCA=tca1,MCA=aim_adc2,CARD=0,SERVER=icbTca/1,ADDR=0")
 
 iocInit()
