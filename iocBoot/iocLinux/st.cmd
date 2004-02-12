4d811cd8edc4d818855951fa3f1ef2d78c879b9e
Mark Rivers <MarkRivers@users.noreply.github.com>
2004-02-12 04:47:54 +0000

    Cleaned up

--- iocBoot/iocLinux/st.cmd
+++ iocBoot/iocLinux/st.cmd
@@ -6,21 +6,15 @@
 #setRMRServerDebug 10
 #
 dbLoadDatabase("../../dbd/mcaCanberra.dbd",0,0)
-registerRecordDeviceDriver(pdbbase) 
-#dbLoadRecords("../../../std/stdApp/Db/scan.db","P=mcaTest:,MAXPTS1=2000,MAXPTS2=200,MAXPTS3=20,MAXPTS4=10,MAXPTSH=10")
-dbLoadRecords("../../../std/stdApp/Db/pid_control.db","P=mcaTest:,PID=PID1,INP=test,OUT=test,SCAN=0,KP=1.,KI=1.,KD=0.,LOPR=0,DRVL=0,HOPR=100.,DRVH=100.,PREC=3")
+mcaCanberra_registerRecordDeviceDriver(pdbbase) 
 
 #
 routerInit
 localMessageRouterStart(0)
 
-#setMcaDebug 10 10 10 10
-#setTcaDebug(10)
-#setIcbDebug(10)
-
 #AIMConfig(serverName, ethernetAddress, ADC port, maxChans,
 #          maxSignals, maxSequences, ethernetDevice, queueSize)
-AIMConfig("AIMServ", 0x59e, 1, 2048, 1, 1, "eth1", 1000)
+AIMConfig("AIMServ", 0x59e, 1, 2048, 1, 1, "eth0", 1000)
 mcaAIMShowModules()
 dbLoadRecords("../../mcaApp/Db/mca.db","P=mcaTest:,M=aim_adc1,NCHAN=2048,NUSE=2048,DTYPE=MPF MCA,INP=#C0 S0@AIMServ")
 
@@ -41,3 +35,4 @@ icbTcaConfig("icbTca/1", 0, 0x59e, 8)
 dbLoadRecords("../../../mca/mcaApp/Db/icb_tca.db", "P=13LAB:,TCA=tca1,MCA=aim_adc2,CARD=0,SERVER=icbTca/1,ADDR=0")
 
 iocInit()
+
