74d31935c61f264a79bbe834c8b315571d2daadf
Mark Rivers <MarkRivers@users.noreply.github.com>
2004-07-12 14:14:04 +0000

    Changes for asyn

--- mcaApp/mcaSrc/mca.h
+++ mcaApp/mcaSrc/mca.h
@@ -1,22 +1,37 @@
 /* mca.h -- communication between record and device support for mca record */
 
-#define MSG_ACQUIRE			0
-#define MSG_READ			1
-#define MSG_SET_CHAS_INT		2
-#define MSG_SET_CHAS_EXT		3
-#define MSG_SET_NCHAN			4
-#define MSG_SET_DWELL			5
-#define MSG_SET_REAL_TIME		6
-#define MSG_SET_LIVE_TIME		7
-#define MSG_SET_COUNTS			8
-#define MSG_SET_LO_CHAN			9
-#define MSG_SET_HI_CHAN			10
-#define MSG_SET_NSWEEPS			11
-#define MSG_SET_MODE_PHA		12
-#define MSG_SET_MODE_MCS		13
-#define MSG_SET_MODE_LIST		14
-#define MSG_GET_ACQ_STATUS		15
-#define MSG_STOP_ACQUISITION		16
-#define MSG_ERASE			17
-#define MSG_SET_SEQ			18
-#define MSG_SET_PSCL			19
+#ifndef mcaH
+#define mcaH
+
+typedef struct {
+   double acquiring;
+   double realTime;
+   double liveTime;
+   double totalCounts;
+   double dwellTime;
+} mcaAsynAcquireStatus;
+
+typedef enum { 
+   MSG_ACQUIRE,
+   MSG_READ,
+   MSG_SET_CHAS_INT,
+   MSG_SET_CHAS_EXT,
+   MSG_SET_NCHAN,
+   MSG_SET_DWELL,
+   MSG_SET_REAL_TIME,
+   MSG_SET_LIVE_TIME,
+   MSG_SET_COUNTS,
+   MSG_SET_LO_CHAN,
+   MSG_SET_HI_CHAN,
+   MSG_SET_NSWEEPS,
+   MSG_SET_MODE_PHA,
+   MSG_SET_MODE_MCS,
+   MSG_SET_MODE_LIST,
+   MSG_GET_ACQ_STATUS,
+   MSG_STOP_ACQUISITION,
+   MSG_ERASE,
+   MSG_SET_SEQ,
+   MSG_SET_PSCL
+} mcaCommand;
+
+#endif /* mcaH */
