1cdacd7448252cbf6727ed671e23311bd3371958
Mark Rivers <MarkRivers@users.noreply.github.com>
2005-09-20 17:41:07 +0000

    Changes for asyn

--- iocBoot/iocVxWorks/st.cmd
+++ iocBoot/iocVxWorks/st.cmd
@@ -1,7 +1,5 @@
 # Example vxWorks startup file
-#Following must be added for many board support packages
 
-cd "/corvette/home/epics/3.14/mca/iocBoot/iocVxWorks"
 < ../nfsCommands
 < cdCommands
 
@@ -12,30 +10,43 @@ cd startup
 dbLoadDatabase("../../dbd/mcaCanberra.dbd",0,0)
 registerRecordDeviceDriver(pdbbase) 
 
-dbLoadRecords("../../mcaApp/Db/mca.db","P=mcaTest:,M=aim_adc1,NCHAN=2048,NUSE=2048,DTYPE=MPF MCA,INP=#C0 S0@AIMServ")
+dbLoadRecords("../../mcaApp/Db/mca.db","P=mcaTest:,M=aim_adc1,NCHAN=2048,NUSE=2048,DTYP=asynMCA,INP=@asyn(AIM1/1)")
 dbLoadRecords("../../mcaApp/Db/icb_adc.db", "P=mcaTest:,ADC=adc1,CARD=0,SERVER=icb/1,ADDR=0")
 dbLoadRecords("../../mcaApp/Db/icb_tca.db", "P=mcaTest:,TCA=tca1,MCA=aim_adc1,CARD=0,SERVER=icbTca/1,ADDR=0")
 
-#
-routerInit
-localMessageRouterStart(0)
+# AIMConfig(portName, ethernet_address, portNumber(1 or 2), maxChans,
+#           maxSignals, maxSequences, ethernetDevice)
+AIMConfig("AIM1/1", 0x59e, 1, 2048, 1, 1, "dc0")
+AIMConfig("AIM1/2", 0x59e, 2, 2048, 1, 1, "dc0")
+dbLoadRecords("$(MCA)/mcaApp/Db/mca.db", "P=mcaTest:,M=aim_adc1,DTYP=asynMCA,INP=@asyn(AIM1/1 0),NCHAN=2048")
+dbLoadRecords("$(MCA)/mcaApp/Db/mca.db", "P=mcaTest:,M=aim_adc2,DTYP=asynMCA,INP=@asyn(AIM1/2 0),NCHAN=2048")
 
-#aimDebug=9
-#mcaAIMServerDebug=10
-#mcaRecordDebug=0
-#devMcaMpfDebug=0
-#transformRecordDebug = 0
-#swaitRecordDebug=10
+#icbConfig(portName, module, ethernetAddress, icbAddress, moduleType)
+#   portName to give to this asyn port
+#   ethernetAddress - Ethernet address of module, low order 16 bits
+#   icbAddress - rotary switch setting inside ICB module
+#   moduleType
+#      0 = ADC
+#      1 = Amplifier
+#      2 = HVPS
+#      3 = TCA
+#      4 = DSP
+icbConfig("icbAdc1", 0x59e, 5, 0)
+dbLoadRecords("$(MCA)/mcaApp/Db/icb_adc.db", "P=mcaTest:,ADC=adc1,PORT=icbAdc1")
+icbConfig("icbAmp1", 0x59e, 3, 1)
+dbLoadRecords("$(MCA)/mcaApp/Db/icb_amp.db", "P=mcaTest:,AMP=amp1,PORT=icbAmp1")
+icbConfig("icbHvps1", 0x59e, 2, 2)
+dbLoadRecords("$(MCA)/mcaApp/Db/icb_hvps.db", "P=mcaTest:,HVPS=hvps1,PORT=icbHvps1,LIMIT=1000")
+icbConfig("icbTca1", 0x59e, 8, 3)
+dbLoadRecords("$(MCA)/mcaApp/Db/icb_tca.db", "P=mcaTest:,TCA=tca1,MCA=aim_adc2,PORT=icbTca1")
+#icbConfig("icbDsp1", 0x8058, 0, 4)
+#dbLoadRecords("$(MCA)/mcaApp/Db/icbDsp.db", "P=mcaTest:,DSP=dsp1,PORT=icbDsp1")
 
-#AIMConfig(serverName, ethernetAddress, ADC port, maxChans,
-#          maxSignals, maxSequences, ethernetDevice, queueSize)
-AIMConfig("AIMServ", 0x59e, 1, 2048, 1, 1, "ei0", 1000)
 nmc_show_modules
 
-#icbConfig(serverName, maxModules, address, queueSize)
-icbConfig("icb/1", 10, "NI59E:5", 100)
-
-#icbTcaConfig(serverName, maxModules, address, queueSize)
-icbTcaConfig("icbTca/1", 1, "NI59E:8", 100)
+#mcaRecordDebug = 10
+#asynSetTraceMask "AIM1/1",0,0xff
+#asynSetTraceMask "icbTca1",0,0x13
+#asynSetTraceMask "icbHvps1",0,0xff
 
 iocInit()
