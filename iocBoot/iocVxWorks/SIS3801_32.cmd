a9bbc783a0eedaefee0a1173d5c07572f6fd7d4a
Mark Rivers <MarkRivers@users.noreply.github.com>
2006-10-26 22:36:46 +0000

    Changes in file name and prefix

--- iocBoot/iocVxWorks/SIS3801_32.cmd
+++ iocBoot/iocVxWorks/SIS3801_32.cmd
@@ -2,8 +2,8 @@ STR7201Setup(1,0x90000000,220,6)
 # STR7201Config(card, maxSignals, maxChans, ch1RefEnable)
 STR7201Config(0, 32, 2048, 1)
 dbLoadRecords("$(MCA)/mcaApp/Db/Struck32.db","P=SIS:3801:")
-dbLoadTemplate("SIS3801_32.template")
+dbLoadTemplate("SIS3801_32.substitutions")
 
 /* Scaler record */
-dbLoadRecords("$(MCA)/mcaApp/Db/STR7201scaler.db", "P=SIS:,S=scaler1,C=0")
+dbLoadRecords("$(MCA)/mcaApp/Db/STR7201scaler.db", "P=SIS:3801:,S=scaler1,C=0")
 
