d67ba8c08fa2dfe18763dcb50dca9a981c30123a
Mark Rivers <MarkRivers@users.noreply.github.com>
2006-10-26 22:26:10 +0000

    The 3820 stuff is in separate Register files

--- mcaApp/SISSrc/mcaSISRegister.c
+++ mcaApp/SISSrc/mcaSISRegister.c
@@ -4,7 +4,6 @@
 #include "epicsExport.h"
 
 #include "drvSTR7201.h"
-#include "drvMcaSIS3820Asyn.h"
 
 extern int drvSTR7201Debug;
 extern int devSTR7201Debug;
@@ -41,58 +40,10 @@ static void STR7201ConfigCallFunc(const iocshArgBuf *args)
                   args[2].ival, args[3].ival, args[4].ival);
 }
 
-
-/* iocsh setup function */
-static const iocshArg mcaSIS3820SetupArg0 = { "BaseAddress",iocshArgInt};
-static const iocshArg mcaSIS3820SetupArg1 = { "InterruptVector",iocshArgInt};
-static const iocshArg mcaSIS3820SetupArg2 = { "InterruptLevel",iocshArgInt};
-
-static const iocshArg * const mcaSIS3820SetupArgs[3] =
-{ &mcaSIS3820SetupArg0,
-  &mcaSIS3820SetupArg1,
-  &mcaSIS3820SetupArg2};
-
-static const iocshFuncDef mcaSIS3820SetupFuncDef =
-{"mcaSIS3820Setup",3,mcaSIS3820SetupArgs};
-
-static void mcaSIS3820SetupCallFunc(const iocshArgBuf *args)
-{
-  mcaSIS3820Setup(args[0].ival, args[1].ival, args[2].ival);
-}
-
-/* iocsh config function */
-static const iocshArg mcaSIS3820ConfigArg0 = { "Asyn port name",iocshArgString};
-static const iocshArg mcaSIS3820ConfigArg1 = { "Card",iocshArgInt};
-static const iocshArg mcaSIS3820ConfigArg2 = { "MaxChan",iocshArgInt};
-static const iocshArg mcaSIS3820ConfigArg3 = { "MaxSign",iocshArgInt};
-static const iocshArg mcaSIS3820ConfigArg4 = { "InputMode",iocshArgInt};
-static const iocshArg mcaSIS3820ConfigArg5 = { "OutputMode",iocshArgInt};
-static const iocshArg mcaSIS3820ConfigArg6 = { "LNEPrescale",iocshArgInt};
-
-static const iocshArg * const mcaSIS3820ConfigArgs[7] =
-{ &mcaSIS3820ConfigArg0,
-  &mcaSIS3820ConfigArg1,
-  &mcaSIS3820ConfigArg2,
-  &mcaSIS3820ConfigArg3,
-  &mcaSIS3820ConfigArg4,
-  &mcaSIS3820ConfigArg5,
-  &mcaSIS3820ConfigArg6 };
-
-static const iocshFuncDef mcaSIS3820ConfigFuncDef =
-{"mcaSIS3820Config",7,mcaSIS3820ConfigArgs};
-
-static void mcaSIS3820ConfigCallFunc(const iocshArgBuf *args)
-{
-  mcaSIS3820Config(args[0].sval, args[1].ival, args[2].ival, args[3].ival,
-      args[4].ival, args[5].ival, args[6].ival);
-}
-
 void mcaSISRegister(void)
 {
     iocshRegister(&STR7201ConfigFuncDef,STR7201ConfigCallFunc);
     iocshRegister(&STR7201SetupFuncDef,STR7201SetupCallFunc);
-    iocshRegister(&mcaSIS3820ConfigFuncDef,mcaSIS3820ConfigCallFunc);
-    iocshRegister(&mcaSIS3820SetupFuncDef,mcaSIS3820SetupCallFunc);
 }
 
 epicsExportRegistrar(mcaSISRegister);
