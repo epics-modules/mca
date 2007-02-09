899da45fb1d2ec44bfbf6404716262791f58f86c
Mark Rivers <MarkRivers@users.noreply.github.com>
2007-02-09 20:00:33 +0000

    Added comments, fixed STR7201Config command

--- iocBoot/iocVxWorks/SIS3801_32.cmd
+++ iocBoot/iocVxWorks/SIS3801_32.cmd
@@ -1,6 +1,7 @@
+# STR7201Setup(int maxCards, int baseAddress, int intVector, int intLevel);
 STR7201Setup(1,0x90000000,220,6)
-# STR7201Config(card, maxSignals, maxChans, ch1RefEnable)
-STR7201Config(0, 32, 2048, 1)
+# STR7201Config(int card, int maxSignals, int maxChans, int ch1RefEnable, int softAdvance);
+STR7201Config(0, 32, 2048, 1, 0)
 dbLoadRecords("$(MCA)/mcaApp/Db/Struck32.db","P=SIS:3801:")
 dbLoadTemplate("SIS3801_32.substitutions")
 
