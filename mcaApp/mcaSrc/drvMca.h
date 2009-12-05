0de378e0cbf5805aad3afd8fdf5a33be23a7f4f6
Mark Rivers <MarkRivers@users.noreply.github.com>
2009-12-05 14:33:33 +0000

    Removed mcaCommandStruct and mcaCommands; we now only publish the drvInfo strings that drivers should use; this allows MCA drivers to also support other interfaces, because enum values are no longer defined

--- mcaApp/mcaSrc/drvMca.h
+++ mcaApp/mcaSrc/drvMca.h
@@ -3,38 +3,29 @@
 #ifndef drvMcaH
 #define drvMcaH
 
-#include "mca.h"
-
-typedef struct {
-    mcaCommand command;
-    const char *commandString;
-} mcaCommandStruct;
-
-static mcaCommandStruct mcaCommands[MAX_MCA_COMMANDS] = {
-    {mcaStartAcquire,           "MCA_START_ACQUIRE"}, /* int32, write */
-    {mcaStopAcquire,            "MCA_STOP_ACQUIRE"},  /* int32, write */
-    {mcaErase,                  "MCA_ERASE"},         /* int32, write */
-    {mcaData,                   "MCA_DATA"},          /* int32Array, read/write */
-    {mcaReadStatus,             "MCA_READ_STATUS"},   /* int32, write */
-    {mcaChannelAdvanceInternal, "MCA_CHAS_INTERNAL"}, /* int32, write */
-    {mcaChannelAdvanceExternal, "MCA_CHAS_EXTERNAL"}, /* int32, write */
-    {mcaNumChannels,            "MCA_NUM_CHANNELS"},  /* int32, write */
-    {mcaDwellTime,              "MCA_DWELL_TIME"},    /* float64, write */
-    {mcaPresetLiveTime,         "MCA_PRESET_LIVE"},   /* float64, write */
-    {mcaPresetRealTime,         "MCA_PRESET_REAL"},   /* float64, write */
-    {mcaPresetCounts,           "MCA_PRESET_COUNTS"}, /* float64, write */
-    {mcaPresetLowChannel,       "MCA_PRESET_LOW_CHANNEL"}, /* int32, write */
-    {mcaPresetHighChannel,      "MCA_PRESET_HIGH_CHANNEL"}, /* int32, write */
-    {mcaPresetSweeps,           "MCA_PRESET_SWEEPS"}, /* int32, write */
-    {mcaModePHA,                "MCA_MODE_PHA"},      /* int32, write */
-    {mcaModeMCS,                "MCA_MODE_MCS"},      /* int32, write */
-    {mcaModeList,               "MCA_MODE_LIST"},     /* int32, write */
-    {mcaSequence,               "MCA_SEQUENCE"},      /* int32, write */
-    {mcaPrescale,               "MCA_PRESCALE"},      /* int32, write */
-    {mcaAcquiring,              "MCA_ACQUIRING"},     /* int32, read */
-    {mcaElapsedLiveTime,        "MCA_ELAPSED_LIVE"},  /* float64, read */
-    {mcaElapsedRealTime,        "MCA_ELAPSED_REAL"},  /* float64, read */
-    {mcaElapsedCounts,          "MCA_ELAPSED_COUNTS"} /* float64, read */
-};
+#define mcaStartAcquireString           "MCA_START_ACQUIRE" /* int32, write */
+#define mcaStopAcquireString            "MCA_STOP_ACQUIRE"  /* int32, write */
+#define mcaEraseString                  "MCA_ERASE"         /* int32, write */
+#define mcaDataString                   "MCA_DATA"          /* int32Array, read/write */
+#define mcaReadStatusString             "MCA_READ_STATUS"   /* int32, write */
+#define mcaChannelAdvanceInternalString "MCA_CHAS_INTERNAL" /* int32, write */
+#define mcaChannelAdvanceExternalString "MCA_CHAS_EXTERNAL" /* int32, write */
+#define mcaNumChannelsString            "MCA_NUM_CHANNELS"  /* int32, write */
+#define mcaDwellTimeString              "MCA_DWELL_TIME"    /* float64, write */
+#define mcaPresetLiveTimeString         "MCA_PRESET_LIVE"   /* float64, write */
+#define mcaPresetRealTimeString         "MCA_PRESET_REAL"   /* float64, write */
+#define mcaPresetCountsString           "MCA_PRESET_COUNTS" /* float64, write */
+#define mcaPresetLowChannelString       "MCA_PRESET_LOW_CHANNEL" /* int32, write */
+#define mcaPresetHighChannelString      "MCA_PRESET_HIGH_CHANNEL" /* int32, write */
+#define mcaPresetSweepsString           "MCA_PRESET_SWEEPS" /* int32, write */
+#define mcaModePHAString                "MCA_MODE_PHA"      /* int32, write */
+#define mcaModeMCSString                "MCA_MODE_MCS"      /* int32, write */
+#define mcaModeListString               "MCA_MODE_LIST"     /* int32, write */
+#define mcaSequenceString               "MCA_SEQUENCE"      /* int32, write */
+#define mcaPrescaleString               "MCA_PRESCALE"      /* int32, write */
+#define mcaAcquiringString              "MCA_ACQUIRING"     /* int32, read */
+#define mcaElapsedLiveTimeString        "MCA_ELAPSED_LIVE"  /* float64, read */
+#define mcaElapsedRealTimeString        "MCA_ELAPSED_REAL"  /* float64, read */
+#define mcaElapsedCountsString          "MCA_ELAPSED_COUNTS" /* float64, read */
 
 #endif /* drvMcaH */
