f7105df18f82784b430fcc88e4ba1eceffda5034
Mark Rivers <MarkRivers@users.noreply.github.com>
2011-04-08 21:50:30 +0000

    Add new ch1_ref_enable parameter to mcaSIS3820Config

--- iocBoot/iocVxWorks/SIS3820_32.cmd
+++ iocBoot/iocVxWorks/SIS3820_32.cmd
@@ -1,13 +1,14 @@
 #mcaSIS3820Setup(int baseAddress, int intVector, int intLevel)
-mcaSIS3820Setup(0x94000000, 224, 6)
+mcaSIS3820Setup(0xA8000000, 224, 6)
 #mcaSIS3820Config("Port name",
 #                  card,
 #                  channels,
 #                  signals,
 #                  input mode,
 #                  output mode,
-#                  lne prescale)
-mcaSIS3820Config("mcaSIS3820/1", 0, 2048, 32, 2, 0, 1000000)
+#                  lne prescale
+#                  ch1 ref enable)
+mcaSIS3820Config("mcaSIS3820/1", 0, 100000, 32, 2, 0, 1000000, 1)
 dbLoadTemplate("SIS3820_32.substitutions")
 
 #asynSetTraceIOMask("mcaSIS3820/1",0,2)
