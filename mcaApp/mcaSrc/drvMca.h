e0282e3622e121fdaec092ec8fbd270b9551874a
Mark Rivers <MarkRivers@users.noreply.github.com>
2011-04-24 16:46:37 +0000

    Replaced mcaChannelAdvanceExternal/Internal with mcaChannelAdvanceSource, and mcaModePHA/MCS/List with mcaAcquireMode

--- mcaApp/mcaSrc/drvMca.h
+++ mcaApp/mcaSrc/drvMca.h
@@ -3,13 +3,25 @@
 #ifndef drvMcaH
 #define drvMcaH
 
+/* Note, these enums must agree with the MCA record.  We define them here
+ * for drivers that should not know about the MCA record. */
+enum MCAAcquireMode {
+	mcaAcquireMode_PHA,
+	mcaAcquireMode_MCS,
+	mcaAcquireMode_List
+};
+
+enum MCAChannelAdvanceSource {
+	mcaChannelAdvance_Internal,
+	mcaChannelAdvance_External
+};
+
 #define mcaStartAcquireString           "MCA_START_ACQUIRE" /* int32, write */
 #define mcaStopAcquireString            "MCA_STOP_ACQUIRE"  /* int32, write */
 #define mcaEraseString                  "MCA_ERASE"         /* int32, write */
 #define mcaDataString                   "MCA_DATA"          /* int32Array, read/write */
 #define mcaReadStatusString             "MCA_READ_STATUS"   /* int32, write */
-#define mcaChannelAdvanceInternalString "MCA_CHAS_INTERNAL" /* int32, write */
-#define mcaChannelAdvanceExternalString "MCA_CHAS_EXTERNAL" /* int32, write */
+#define mcaChannelAdvanceSourceString   "MCA_CH_ADV_SOURCE" /* int32, write */
 #define mcaNumChannelsString            "MCA_NUM_CHANNELS"  /* int32, write */
 #define mcaDwellTimeString              "MCA_DWELL_TIME"    /* float64, write */
 #define mcaPresetLiveTimeString         "MCA_PRESET_LIVE"   /* float64, write */
@@ -18,9 +30,7 @@
 #define mcaPresetLowChannelString       "MCA_PRESET_LOW_CHANNEL" /* int32, write */
 #define mcaPresetHighChannelString      "MCA_PRESET_HIGH_CHANNEL" /* int32, write */
 #define mcaPresetSweepsString           "MCA_PRESET_SWEEPS" /* int32, write */
-#define mcaModePHAString                "MCA_MODE_PHA"      /* int32, write */
-#define mcaModeMCSString                "MCA_MODE_MCS"      /* int32, write */
-#define mcaModeListString               "MCA_MODE_LIST"     /* int32, write */
+#define mcaAcquireModeString            "MCA_ACQUIRE_MODE"  /* int32, write */
 #define mcaSequenceString               "MCA_SEQUENCE"      /* int32, write */
 #define mcaPrescaleString               "MCA_PRESCALE"      /* int32, write */
 #define mcaAcquiringString              "MCA_ACQUIRING"     /* int32, read */
