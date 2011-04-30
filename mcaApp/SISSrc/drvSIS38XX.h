0e76109a4bcee5085e96e146fcc1c24bcc3e2789
Mark Rivers <MarkRivers@users.noreply.github.com>
2011-04-30 22:47:06 +0000

    Numerous changes during initial development

--- mcaApp/SISSrc/drvSIS38XX.h
+++ mcaApp/SISSrc/drvSIS38XX.h
@@ -1,5 +1,5 @@
 /* File:    drvSIS38XX.h
- * Author:  Mark Rivers
+ * Author:  Mark Rivers, University of Chicago
  * Date:    22-Apr-2011
  *
  * Purpose: 
@@ -23,29 +23,41 @@
 #include <epicsEvent.h>
 #include <epicsTypes.h>
 
-// My temporary file
-#include <vmeDMA.h>
 
 
 /***************/
 /* Definitions */
 /***************/
 
-#define SIS38XXLEDString              "SIS38XX_LED"
-#define SIS38XXMuxOutString           "SIS38XX_MUX_OUT"
-#define SIS38XXCh1RefEnableString     "SIS38XX_CH1_REF_ENABLE"
-#define SIS38XXAcquireModeString      "SIS38XX_ACQUIRE_MODE"
-#define SIS38XXInputModeString        "SIS38XX_INPUT_MODE"
-#define SIS38XXOutputModeString       "SIS38XX_OUTPUT_MODE"
-#define SIS38XXModelString            "SIS38XX_MODEL"
-#define SIS38XXFirmwareString         "SIS38XX_FIRMWARE"
+#define SIS38XXLEDString                    "SIS38XX_LED"
+#define SIS38XXMuxOutString                 "SIS38XX_MUX_OUT"
+#define SIS38XXChannel1SourceString         "SIS38XX_CHANNEL1_SOURCE"
+#define SIS38XXMaxChannelsString            "SIS38XX_MAX_CHANNELS"
+#define SIS38XXCurrentChannelString         "SIS38XX_CURRENT_CHANNEL"
+#define SIS38XXAcquireModeString            "SIS38XX_ACQUIRE_MODE"
+#define SIS38XXInputModeString              "SIS38XX_INPUT_MODE"
+#define SIS38XXOutputModeString             "SIS38XX_OUTPUT_MODE"
+#define SIS38XXSoftwareChannelAdvanceString "SIS38XX_SOFTWARE_CHANNEL_ADVANCE"
+#define SIS38XXInitialChannelAdvanceString  "SIS38XX_INITIAL_CHANNEL_ADVANCE"
+#define SIS38XXModelString                  "SIS38XX_MODEL"
+#define SIS38XXFirmwareString               "SIS38XX_FIRMWARE"
 
 #define SIS38XX_MAX_SIGNALS 32
 
 typedef enum {
-    MCS_MODE,
-    SCALER_MODE
-} SIS38XXAcquireMode;
+    ACQUIRE_MODE_MCS,
+    ACQUIRE_MODE_SCALER
+} SIS38XXAcquireMode_t;
+
+typedef enum {
+    CHANNEL1_SOURCE_INTERNAL,
+    CHANNEL1_SOURCE_EXTERNAL
+} SIS38XXChannel1Source_t;
+
+typedef enum {
+    MODEL_SIS3801,
+    MODEL_SIS3820
+} SIS38XXModel_t;
 
 class drvSIS38XX : public asynPortDriver
 {
@@ -59,18 +71,16 @@ class drvSIS38XX : public asynPortDriver
   asynStatus readInt32Array(asynUser *pasynUser, epicsInt32 *data, 
                                     size_t maxChans, size_t *nactual);
   virtual void report(FILE *fp, int details);
-  virtual void dmaCallback();    // Should be private, but called from C callback function
-  bool checkDone(int fifoCount);
-  virtual void erase();
-
+  
   protected:
+  virtual void checkMCSDone();
+  virtual void erase();
   // Pure virtual functions, derived class must implement these
   virtual void stopMCSAcquire() = 0;
   virtual void startMCSAcquire() = 0;
-  virtual void setChannelAdvanceSource() = 0;
   virtual void enableInterrupts() = 0;
   virtual void disableInterrupts() = 0;
-  virtual void setAcquireMode(SIS38XXAcquireMode acquireMode) = 0;
+  virtual void setAcquireMode(SIS38XXAcquireMode_t acquireMode) = 0;
   virtual void resetScaler() = 0;
   virtual void startScaler() = 0;
   virtual void stopScaler() = 0;
@@ -79,6 +89,7 @@ class drvSIS38XX : public asynPortDriver
   virtual void setScalerPresets() = 0;
   virtual void setOutputMode() = 0;
   virtual void setInputMode() = 0;
+  virtual void softwareChannelAdvance() = 0;
   virtual void setLED() = 0;
   virtual int getLED() = 0;
   virtual void setMuxOut() = 0;
@@ -114,28 +125,27 @@ class drvSIS38XX : public asynPortDriver
   int scalerDone_;
   int SIS38XXLED_;
   int SIS38XXMuxOut_;
-  int SIS38XXCh1RefEnable_;
+  int SIS38XXChannel1Source_;
+  int SIS38XXMaxChannels_;
+  int SIS38XXCurrentChannel_;
   int SIS38XXAcquireMode_;
   int SIS38XXInputMode_;
   int SIS38XXOutputMode_;
+  int SIS38XXSoftwareChannelAdvance_;
+  int SIS38XXInitialChannelAdvance_;
   int SIS38XXModel_;
   int SIS38XXFirmware_;
   #define LAST_SIS38XX_PARAM SIS38XXFirmware_
 
   bool exists_;
-  int moduleID_;
   epicsUInt32 *fifo_base_;
   epicsUInt32 irqStatusReg_;
-  SIS38XXAcquireMode acquireMode_;
+  SIS38XXAcquireMode_t acquireMode_;
   int maxSignals_;
   int maxChans_;
   epicsTimeStamp startTime_;
-  double elapsedTime_;
   double elapsedPrevious_;
   bool erased_;
-  int lneSource_;
-  int lnePrescale_;
-  int softAdvance_;
   epicsUInt32 *mcsData_;     /* maxSignals * maxChans */
   epicsUInt32 *scalerData_;  /* maxSignals */
   int nextChan_;
@@ -144,11 +154,7 @@ class drvSIS38XX : public asynPortDriver
   int fifoBufferWords_;
   epicsUInt32 *fifoBuffPtr_;
   bool acquiring_;
-  bool prevAcquiring_;
   epicsEventId readFIFOEventId_;
-  bool useDma_;
-  DMA_ID dmaId_;
-  epicsEventId dmaDoneEventId_;
   epicsMutexId fifoLockId_;
 };
 
