9fb4be0876d4f53fc0b1541894d02cc447fc157d
Tim Mooney <timmmooney@users.noreply.github.com>
2012-08-17 17:13:08 +0000

    removed tsDefs.h - not needed and not in EPICS 3.15.0

--- mcaApp/mcaSrc/mcaRecord.c
+++ mcaApp/mcaSrc/mcaRecord.c
@@ -106,7 +106,7 @@
 #include    <recSup.h>
 #include    <recGbl.h>
 #include    <special.h>
-#include    <tsDefs.h>
+/* not in 3.15.0.1 #include    <tsDefs.h> */
 #include    <menuYesNo.h> 
 
 #define GEN_SIZE_OFFSET
@@ -806,8 +806,8 @@ read_data:
     if (!pmca->acqg) {
         /* Use TimeStamp to record end of acquisition */
         recGblGetTimeStamp(pmca);
-        tsStampToText(&pmca->time, TS_TEXT_MONDDYYYY,
-                       pmca->stim);
+		epicsTimeToStrftime(pmca->stim, 25, "%b %d, %Y %T.%03f", &pmca->time);
+        /* tsStampToText(&pmca->time, TS_TEXT_MONDDYYYY, pmca->stim);*/
         /* Trim STIM to 25 characters = .001 sec precision */
         pmca->stim[25]='\0';
         MARK(M_STIM);
