1c9944a1109a41544b68c355d849ad5ae579880c
Mark Rivers <rivers@cars.uchicago.edu>
2017-08-01 08:18:23 -0500

    Just calls st_base.cmd

--- iocBoot/iocAmptek/st.cmd
+++ iocBoot/iocAmptek/st.cmd
@@ -1,22 +1,2 @@
 < envPaths
-epicsEnvSet(STARTUP,$(TOP)/iocBoot/$(IOC))
-
-dbLoadDatabase("../../dbd/mcaAmptekApp.dbd",0,0)
-mcaAmptekApp_registerRecordDeviceDriver(pdbbase) 
-
-drvAmptekConfigure(Amptek1, 0, "164.54.160.255", 16347)
-
-#asynSetTraceMask(Amptek1, 0, 255)
-#asynSetTraceIOMask(Amptek1, 0, 2)
-
-dbLoadRecords("../../mcaApp/Db/mca.db","P=mcaTest:,M=mca1,NCHAN=1024,DTYP=asynMCA,INP=@asyn(Amptek1)")
-
-dbLoadRecords("$(ASYN)/db/asynRecord.db","P=mcaTest:,R=serial1,PORT=serial1,ADDR=0,OMAX=256,IMAX=256")
-
-< save_restore.cmd
-
-iocInit()
-
-# save settings every thirty seconds
-create_monitor_set("auto_settings.req",30,"P=mcaTest:")
-
+< st_base.cmd
