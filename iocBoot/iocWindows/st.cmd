6a56b9b9972533f9d65e6bf05fdcf27573648354
Mark Rivers <rivers@cars.uchicago.edu>
2019-11-25 17:45:33 -0600

    Use PREFIX; change interface

--- iocBoot/iocWindows/st.cmd
+++ iocBoot/iocWindows/st.cmd
@@ -4,7 +4,11 @@ epicsEnvSet(STARTUP,$(TOP)/iocBoot/$(IOC))
 dbLoadDatabase("../../dbd/mcaCanberra.dbd",0,0)
 mcaCanberra_registerRecordDeviceDriver(pdbbase) 
 
-#var aimDebug 10
+errlogInit(20000)
+
+var aimDebug 20
+
+epicsEnvSet PREFIX 13LAB:
 
 # AIMConfig(portName, ethernet_address, portNumber(1 or 2), maxChans,
 #           maxSignals, maxSequences, ethernetDevice)
@@ -16,21 +20,19 @@ mcaCanberra_registerRecordDeviceDriver(pdbbase)
 # Canberra hardware is attached to, and replace the string in the AIMConfig
 # commands below with that string.
 
-# This is my office 32-bit computer
-#AIMConfig("AIM1/1", 0x59e, 1, 2048, 1, 1, "\Device\NPF_{5EDD2D78-B567-49D2-B7C3-9340BE526C25}")
-#AIMConfig("AIM1/2", 0x59e, 2, 2048, 1, 1, "\Device\NPF_{5EDD2D78-B567-49D2-B7C3-9340BE526C25}")
-# This is my office 64-bit computer
-AIMConfig("AIM1/1", 0x59e, 1, 2048, 1, 1, "\Device\NPF_{73750E33-093D-41D2-8915-CA2078FE8E88}")
-AIMConfig("AIM1/2", 0x59e, 2, 2048, 1, 1, "\Device\NPF_{73750E33-093D-41D2-8915-CA2078FE8E88}")
-# This is the lab computer
-#AIMConfig("AIM1/1", 0x59e, 1, 2048, 1, 1, "\Device\NPF_{864AA927-88D6-4407-96F5-A5DF9E43D684}")
-#AIMConfig("AIM1/2", 0x59e, 2, 2048, 1, 1, "\Device\NPF_{864AA927-88D6-4407-96F5-A5DF9E43D684}")
-# This is the lab 64-bit computer
-#AIMConfig("AIM1/1", 0x59e, 1, 2048, 1, 1, "\Device\NPF_{FBAE6033-0C1B-4166-A3B4-C377270DE4A3}")
-#AIMConfig("AIM1/2", 0x59e, 2, 2048, 1, 1, "\Device\NPF_{FBAE6033-0C1B-4166-A3B4-C377270DE4A3}")
+# AIMConfig(portName, ethernet_address, portNumber(1 or 2), maxChans, 
+#           maxSignals, maxSequences, ethernetDevice)
+AIMConfig("AIM1/1", 0x3ED, 1, 2048, 1, 1, " \Device\NPF_{3BD8078E-A967-48D1-8B49-A21FA38B19F0}")
+AIMConfig("AIM1/2", 0x3ED, 2, 2048, 8, 1, " \Device\NPF_{3BD8078E-A967-48D1-8B49-A21FA38B19F0}")
+#AIMConfig("DSA2000", 0x8058, 1, 2048, 1, 1, "fei0")
+dbLoadRecords("$(MCA)/mcaApp/Db/mca.db", "P=$(PREFIX),M=aim_adc1,DTYP=asynMCA,INP=@asyn(AIM1/1 0),NCHAN=2048")
+dbLoadRecords("$(MCA)/mcaApp/Db/mca.db", "P=$(PREFIX),M=aim_adc2,DTYP=asynMCA,INP=@asyn(AIM1/2 0),NCHAN=2048")
+dbLoadRecords("$(MCA)/mcaApp/Db/mca.db", "P=$(PREFIX),M=aim_adc3,DTYP=asynMCA,INP=@asyn(AIM1/2 2),NCHAN=2048")
+dbLoadRecords("$(MCA)/mcaApp/Db/mca.db", "P=$(PREFIX),M=aim_adc4,DTYP=asynMCA,INP=@asyn(AIM1/2 4),NCHAN=2048")
+dbLoadRecords("$(MCA)/mcaApp/Db/mca.db", "P=$(PREFIX),M=aim_adc5,DTYP=asynMCA,INP=@asyn(AIM1/2 6),NCHAN=2048")
+#dbLoadRecords("$(MCA)/mcaApp/Db/mca.db", "P=$(PREFIX),M=dsa2000_1,DTYP=asynMCA,INP=@asyn(DSA2000 0),NCHAN=2048")
 
-dbLoadRecords("$(MCA)/mcaApp/Db/mca.db", "P=mcaTest:,M=aim_adc1,DTYP=asynMCA,INP=@asyn(AIM1/1 0),NCHAN=2048")
-dbLoadRecords("$(MCA)/mcaApp/Db/mca.db", "P=mcaTest:,M=aim_adc2,DTYP=asynMCA,INP=@asyn(AIM1/2 0),NCHAN=2048")
+icbShowModules
 
 #icbConfig(portName, module, ethernetAddress, icbAddress, moduleType)
 #   portName to give to this asyn port
@@ -42,18 +44,20 @@ dbLoadRecords("$(MCA)/mcaApp/Db/mca.db", "P=mcaTest:,M=aim_adc2,DTYP=asynMCA,INP
 #      2 = HVPS
 #      3 = TCA
 #      4 = DSP
-icbConfig("icbAdc1", 0x59e, 5, 0)
-dbLoadRecords("$(MCA)/mcaApp/Db/icb_adc.db", "P=mcaTest:,ADC=adc1,PORT=icbAdc1")
-icbConfig("icbAmp1", 0x59e, 3, 1)
-dbLoadRecords("$(MCA)/mcaApp/Db/icb_amp.db", "P=mcaTest:,AMP=amp1,PORT=icbAmp1")
-icbConfig("icbHvps1", 0x59e, 2, 2)
-dbLoadRecords("$(MCA)/mcaApp/Db/icb_hvps.db", "P=mcaTest:,HVPS=hvps1,PORT=icbHvps1,LIMIT=1000")
-icbConfig("icbTca1", 0x59e, 8, 3)
-dbLoadRecords("$(MCA)/mcaApp/Db/icb_tca.db", "P=mcaTest:,TCA=tca1,MCA=aim_adc2,PORT=icbTca1")
+icbConfig("icbAdc1", 0x3ED, 5, 0)
+dbLoadRecords("$(MCA)/mcaApp/Db/icb_adc.db", "P=$(PREFIX),ADC=adc1,PORT=icbAdc1")
+icbConfig("icbAmp1", 0x3ED, 3, 1)
+dbLoadRecords("$(MCA)/mcaApp/Db/icb_amp.db", "P=$(PREFIX),AMP=amp1,PORT=icbAmp1")
+icbConfig("icbHvps1", 0x3ED, 2, 2)
+dbLoadRecords("$(MCA)/mcaApp/Db/icb_hvps.db", "P=$(PREFIX),HVPS=hvps1,PORT=icbHvps1,LIMIT=6000")
+icbConfig("icbTca1", 0x3ED, 8, 3)
+dbLoadRecords("$(MCA)/mcaApp/Db/icb_tca.db", "P=$(PREFIX),TCA=tca1,MCA=aim_adc1,PORT=icbTca1")
 #icbConfig("icbDsp1", 0x8058, 0, 4)
-#dbLoadRecords("$(MCA)/mcaApp/Db/icbDsp.db", "P=mcaTest:,DSP=dsp1,PORT=icbDsp1")
+#dbLoadRecords("$(MCA)/mcaApp/Db/icbDsp.db", "P=$(PREFIX),DSP=dsp1,PORT=icbDsp1")
+
 
 mcaAIMShowModules
+
 
 #asynSetTraceMask "AIM1/1",0,0xff
 #asynSetTraceMask "icbTca1",0,0x13
@@ -64,5 +68,5 @@ mcaAIMShowModules
 iocInit()
 
 # save settings every thirty seconds
-create_monitor_set("auto_settings.req",30,"P=mcaTest:")
+create_monitor_set("auto_settings.req",30,"P=$(PREFIX)")
 
