7ae10a5256296b72421e217c3ed24a88756a95cb
Mark Rivers <rivers@cars.uchicago.edu>
2022-06-20 16:48:13 -0500

    Use single save_restore.cmd for most IOCs

--- iocBoot/save_restore.cmd
+++ iocBoot/save_restore.cmd
@@ -7,7 +7,7 @@
 #save_restoreSet_NFSHost("oxygen", "164.54.52.4")
 
 # status-PV prefix
-#save_restoreSet_status_prefix("SIS:")
+save_restoreSet_status_prefix("mcaTest:")
 # Debug-output level
 save_restoreSet_Debug(0)
 
@@ -22,15 +22,22 @@ save_restoreSet_NumSeqFiles(3)
 save_restoreSet_SeqPeriodInSeconds(300)
 
 # specify where save files should be
-set_savefile_path("./autosave")
+set_savefile_path($(STARTUP), "autosave")
+
+# specify what save files should be restored.  Note these files must be
+# in the directory specified in set_savefile_path(), or, if that function
+# has not been called, from the directory current when iocInit is invoked
+set_pass0_restoreFile("auto_settings.sav")
+set_pass1_restoreFile("auto_settings.sav")
 
 # specify directories in which to to search for included request files
 # Note cdCommands defines 'startup', but envPaths does not
-set_requestfile_path("./")
-set_requestfile_path($(AUTOSAVE), "asApp/Db")
-set_requestfile_path($(CALC),     "calcApp/Db")
-set_requestfile_path($(MCA),      "mcaApp/Db")
-set_requestfile_path($(SSCAN),    "sscanApp/Db")
-set_requestfile_path($(SCALER),      "db")
+set_requestfile_path($(STARTUP),  "")
+set_requestfile_path($(STARTUP),  "autosave")
+set_requestfile_path($(AUTOSAVE), "db")
+set_requestfile_path($(CALC),     "db")
+set_requestfile_path($(MCA),      "db")
+set_requestfile_path($(SSCAN),    "db")
+set_requestfile_path($(SCALER),   "db")
 
-#dbLoadRecords("$(AUTOSAVE)/asApp/Db/save_restoreStatus.db", "P=SIS:")
+dbLoadRecords("$(AUTOSAVE)/db/save_restoreStatus.db", "P=mcaTest:")
